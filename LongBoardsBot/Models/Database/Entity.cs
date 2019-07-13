6eca4819ef8ce512336203efe171d761f2b31baa
Anton Ze! <zelenkovanton523@gmail.com>
2019-07-13 12:43:45 +0300

    added image drawing demo

---
+++ LongBoardsBot/Models/Database/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace LongBoardsBot.Models.Database
+{
+    public class Entity
+    {
+        public long Id { get; set; }
+    }
+}
---
+++ LongBoardsBot/Models/Database/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LongBoardsBot.Models.Database
+{
+    public class User : Entity
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public IEnumerable<LongBoard> BoughtLongBoards { get; set; }
+    }
+}
